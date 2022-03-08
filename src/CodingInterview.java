a0d546a029db36ade8ec410f18e958af01fbb2e0
pranavchati <pchati2003@gmail.com>
2022-03-07 19:06:20 -0500

    Added a while loop that helps read through the entire text file and segments the various commands that are called

--- src/CodingInterview.java
+++ src/CodingInterview.java
@@ -1,64 +1,51 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-import components.simplereader.SimpleReader;
-import components.simplereader.SimpleReader1L;
-import components.simplewriter.SimpleWriter;
-import components.simplewriter.SimpleWriter1L;
-
 public final class CodingInterview {
 
-    public static void main(String[] args) {
-        //declare a scanner
-        Scanner commandLineInput = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        //declare a scanner for file input
+        Scanner cmdLineInput = new Scanner(System.in);
+        BufferedReader inputFile = null;
 
         System.out.println("Enter Input File: ");
-
-        SimpleReader in = new SimpleReader1L();
-        SimpleWriter out = new SimpleWriter1L();
-
-        //ask the user for the input file
-
-        out.println("Enter Input File: ");
-        String inputFileName = in.nextLine();
+        String inputFileName = cmdLineInput.nextLine();
 
         try {
-            BufferedWriter write = new BufferedWriter(
-                    new FileWriter(inputFileName));
+            inputFile = new BufferedReader(new FileReader(inputFileName));
+            String line;
+            while ((line = inputFile.readLine()) != null) {
+                String[] commands = line.split(" ");
+                switch (commands[0].toLowerCase()) {
+                    case "partner":
+                        System.out.println("Partner " + commands[1]);
+                        break;
+
+                    case "company":
+                        System.out.println("Company");
+                        break;
+
+                    case "employee":
+                        System.out.println("Employee");
+                        break;
+
+                    case "contact":
+                        System.out.println("Contact");
+                        break;
+
+                    default:
+                        System.out.println("This command does not work");
+                        break;
+
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //out.println("Enter OutputFile: ");
-        // String outputFile = in.nextLine();
-
-        //declare a file to write too
-        SimpleReader inputFile = new SimpleReader1L(inputFileName);
-
-        while (!inputFile.atEOS()) {
-            String text = inputFile.nextLine();
-            String[] values = text.split(" ");
-
-            switch (values[0]) {
-                case "Partner":
-                    out.print("Partner");
-                    break;
-
-                case "Company":
-                    out.print("Company");
-                    break;
-
-                default:
-                    out.print("Nothing");
-                    break;
-
-            }
-        }
-
-        in.close();
-        inputFile.close();
-        out.close();
+        cmdLineInput.close();
     }
 }
